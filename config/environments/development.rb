2f10aa0b360d6088ce35a987909fb366dc53f7ab
Charles <jslack2537@gmail.com>
2019-09-13 19:16:43 +0000

    generated devise views and users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -30,6 +31,8 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+
+  config.action_mailer.default_url_options = {host: 'localhost:3030'}
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
