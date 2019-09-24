7b2478cfe32e23f21f9bbcc8b6aeabcb518a11db
Charles <jslack2537@gmail.com>
2019-09-24 13:40:24 +0000

    fixed another bug

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -14,9 +14,9 @@ class Instructor::SectionsController < ApplicationController
 
   private
 
-  def require_authorized_for_current_course
+   def require_authorized_for_current_course
     if current_course.user != current_user
-      render plain: "Unauthorized", status :unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
 
