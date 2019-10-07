f90d0af9c13b84b468ab34200d1d3b11f7de2752
Joe Slack <jslack2537@gmail.com>
2019-10-07 13:17:49 +0000

    added permissons for enrolled users

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'mini_magick'
 
 gem 'carrierwave-aws'
 
+gem 'stripe'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,24 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_lesson
+
+
   def show
   end
 
-
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.user != current_user
+      redirect_to course_sections, alert: 'Error Message Here'
+    end
+  end
+
+  def require_enrollment_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: "You must enroll."
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
