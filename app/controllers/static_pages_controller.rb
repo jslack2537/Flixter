7dff0f89e0d233295b2dd420225550f65954887d
Joe Slack <jslack2537@gmail.com>
2019-10-10 17:08:08 +0000

    created static pages fo rthe about heading

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
   def index 
   end
+
+  def privacy
+  end
+
+  def team
+  end
+
+  def career
+  end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'career', to: 'static_pages#career'
+  get 'team', to: 'static_pages#team'
+  get 'about', to: 'static_pages#about'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
